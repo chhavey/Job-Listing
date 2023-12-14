54007b6e2ba28a296d5115a9328c777e86cb261d
Chhavi <guptachhavi880@gmail.com>
2023-12-14 18:47:31 +0530

    added database config and initialzation

---
+++ server/index.js
@@ -0,0 +1,20 @@
+const express = require('express')
+const bodyParser = require('body-parser')
+const dotenv = require('dotenv');
+dotenv.config();
+const mongoose = require('mongoose')
+
+const app = express();
+
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.json())
+
+app.get('/health', (req, res) => {
+    res.send('Server is up and running.');
+})
+
+app.listen(process.env.PORT, () => {
+    mongoose.connect(process.env.MONGODB_URL)
+        .then(() => console.log(`Server running on port ${process.env.PORT}`))
+        .catch((error) => console.log(error.message))
+})
